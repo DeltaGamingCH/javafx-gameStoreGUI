67d94d1cd7b543998639666d07466ca2ea051740
deltagamingch <deltagaming.ch.nik@gmail.com>
2024-06-11 09:19:32 +0200

    Added blank stylesheet, attempt to add sidebar

--- src/main/java/project/App.java
+++ src/main/java/project/App.java
@@ -16,6 +16,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("primary"), 640, 480);
+        scene.getStylesheets().add("stylesheet.css");
         stage.setScene(scene);
         stage.show();
     }
