7e999579afb82f904063f429b8e6fa8b762c2c60
deltagamingch <deltagaming.ch.nik@gmail.com>
2024-06-17 21:26:55 +0200

    Created redirect to login, fixed return from login page

--- src/main/java/project/InitialController.java
+++ src/main/java/project/InitialController.java
@@ -1,6 +1,8 @@
 package project;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import java.io.IOException;
@@ -22,7 +24,15 @@ public class InitialController {
         });
     }
 
-    public ImageView getImageView() {
-        return imageView;
+    @FXML
+    private Button login;
+
+    @FXML
+    private void login(ActionEvent event) throws IOException {
+        App.setRoot("login");
     }
+
+    /*public ImageView getImageView() {
+        return imageView;
+    }*/
 }
